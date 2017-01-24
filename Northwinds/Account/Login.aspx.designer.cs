7cb58612c52bed9891a4ced0e0c6d1790abe6f74
JODY MC LAUGHLIN <jmclaughlin31@qub.ac.uk>
2017-01-24 17:37:43 +0000

    Updates for Logon and Resetting Password via Email

--- Northwinds/Account/Login.aspx.designer.cs
+++ Northwinds/Account/Login.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace Northwinds.Account {
         protected global::System.Web.UI.WebControls.CheckBox RememberMe;
         
         /// <summary>
+        /// ResendConfirm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ResendConfirm;
+        
+        /// <summary>
         /// RegisterHyperLink control.
         /// </summary>
         /// <remarks>
