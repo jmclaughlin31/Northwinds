3cca6096ef792b9abaaf13f522ca3458df5b9dc3
JODY MC LAUGHLIN <jmclaughlin31@qub.ac.uk>
2017-01-27 16:26:28 +0000

    commit

--- Northwinds/Admin/Dashboard.aspx.designer.cs
+++ Northwinds/Admin/Dashboard.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace Northwinds.Admin {
     
     
     public partial class Dashboard {
+        
+        /// <summary>
+        /// report control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Microsoft.PowerBI.AspNet.WebForms.UI.WebControls.Report report;
     }
 }
